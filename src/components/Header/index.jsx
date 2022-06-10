ad46d3dff863985cac674f62968936c78f93d718
thamybessa <thamybessa@dcc.ufrj.br>
2022-06-10 13:38:13 -0700

    Add Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import PillInputButton from "../PillInputButton";
 import WALogo from "./../../img/logo.png";
 import styles from "./index.module.css";
 
 const Header = () => {
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const today = new Date().toLocaleDateString("en-US", dateOptions);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (value) => setSearchValue(value);
+
   return (
-    <header>
-      <img src={WALogo} alt="Weather App logo" className={styles.logo} />
+    <header className={`header ${styles.header}`}>
+      <div className={styles.container}>
+        <img src={WALogo} alt="Weather App logo" className={styles.logo} />
+        <span>{today}</span>
+      </div>
+      <PillInputButton
+        value={searchValue}
+        onChangeInput={handleSearchChange}
+        placeholder="Enter city name"
+        btnText="Search"
+      />
     </header>
   );
 };
